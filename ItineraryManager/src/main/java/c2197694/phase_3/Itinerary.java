fcc80fa210446f1544e77f2745a8d8357a2e11e2
DrEpico <76069708+DrEpico@users.noreply.github.com>
2024-01-10 04:25:30 +0000

    Added activities and addons

--- ItineraryManager/src/main/java/c2197694/phase_3/Itinerary.java
+++ ItineraryManager/src/main/java/c2197694/phase_3/Itinerary.java
@@ -60,6 +60,8 @@ public class Itinerary {
     private final AddOn accommodation = new AddOn("Accommodation", 1700, "itinerary", "ACC");
     private final AddOn teaBreaks = new AddOn("Tea break", 500, "itinerary", "TEA");
     private final AddOn lunch = new AddOn("Lunch", 1500, "itinerary", "LUN");
+    private final AddOn nightEntertainment = new AddOn("Night Entertainment Package", 1900, "itinerary", "NEP");
+    private final AddOn explorerJournal = new AddOn("Explorer's Journal Kit", 800, "itinerary", "EJK");
 
     private final Scanner scanner = new Scanner(System.in);
 
@@ -82,6 +84,8 @@ public class Itinerary {
         this.addDisplayAddOns(accommodation);
         this.addDisplayAddOns(teaBreaks);
         this.addDisplayAddOns(lunch);
+        this.addDisplayAddOns(nightEntertainment);
+        this.addDisplayAddOns(explorerJournal); 
     }
 
     /**
